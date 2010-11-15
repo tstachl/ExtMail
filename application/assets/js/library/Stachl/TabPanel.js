a86f30265dcab16d21d0a3c812cdf81db69d0a3e
Thomas Stachl <thomas@stachl.me>
2010-11-15 08:47:39 +0100

    Removed debug message on click at the tab strip

--- application/assets/js/library/Stachl/TabPanel.js
+++ application/assets/js/library/Stachl/TabPanel.js
@@ -1,7 +1,6 @@
 Ext.ns('Stachl');
 Stachl.TabPanel = Ext.extend(Ext.TabPanel, {
 	onStripMouseDown: function(e) {
-		Debug.log(e);
 		Stachl.TabPanel.superclass.onStripMouseDown.call(this, e);
 		if ((Ext.isGecko && e.button !== 1) || (Ext.isWebkit && e.button !== 2)) {
 			return;
